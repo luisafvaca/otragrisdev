a53eb7355d681ffdc2444dce7eeaa4d2f822e30b
Luisa Vaca <luisavaca@Luisas-iMac.local>
2020-03-28 11:21:49 -0500

    otra-dev: initial commit

---
+++ src/components/Landing/landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './landing.css';
+
+class Landing extends React.Component {
+    render(){
+        return (
+            <section className="landing-container">
+                <div>Hola soy Luisa Vaca! 👩🏻‍🦰</div>
+                <div>Este sitio está en construcción, disculpa las molestias.</div>
+            </section>
+        )
+    }
+}
+
+export default Landing;
